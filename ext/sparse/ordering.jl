95ded31aa983de45d8c99182d9b2ae1952ca9dd1
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-10-13 15:10:06 +0200

    Add sparse ordering for matrix representation

--- ext/sparse/ordering.jl
+++ ext/sparse/ordering.jl
@@ -18,3 +18,41 @@ function construct_ordering(T, p::VVR) where {VVR <: AbstractVector{<:AbstractSp
 
     return SparseOrdering(perm, state_to_subset, subsets)
 end
+
+# Sparse matrix
+function construct_ordering(T, p::AbstractSparseMatrix)
+    # Assume that input/start state is on the columns and output/target state is on the rows
+    n, m = size(p)
+    perm = collect(UnitRange{T}(1, n))
+    state_to_subset = construct_state_to_subset(T, n)
+
+    subsets = Vector{PermutationSubset{T, Vector{T}}}(undef, m)
+    for j in eachindex(subsets)
+        pⱼ = view(p, :, j)
+        subsets[j] = PermutationSubset(1, Vector{T}(undef, nnz(pⱼ)))
+
+        ids = SparseArrays.nonzeroinds(pⱼ)  # This is not exported, but we need the non-zero indices
+        for i in ids
+            push!(state_to_subset[i].index, j)
+        end
+    end
+
+    return SparseOrdering(perm, state_to_subset, subsets)
+    n = size(p, 1)
+    return DenseOrdering{T}(n)
+end
+
+function construct_state_to_subset(T, n)
+    state_to_subset = Vector{Vector{T}}(undef, n)
+    for i in eachindex(state_to_subset)
+        state_to_subset[i] = Int64[]
+    end
+
+    return state_to_subset
+end
+
+function sort_states!(order::DenseOrdering, V; max = true)
+    sortperm!(order.perm, V; rev = max)  # rev=true for maximization
+
+    return order
+end
--- test/sparse.jl
+++ test/sparse.jl
@@ -15,3 +15,15 @@ V = collect(1:15)
 p = ominmax(prob, V; max = true)
 @test p[1] ≈ SparseVector(15, [4, 10], [0.3, 0.7])
 @test p[2] ≈ SparseVector(15, [5, 6, 7], [0.5, 0.3, 0.2])
+
+# Matrix
+prob = MatrixIntervalProbabilities(;
+    lower = sparse_hcat(SparseVector(15, [4, 10], [0.1, 0.2]), SparseVector(15, [5, 6, 7], [0.5, 0.3, 0.1])),
+    upper = sparse_hcat(SparseVector(15, [1, 4, 10], [0.5, 0.6, 0.7]), SparseVector(15, [5, 6, 7], [0.7, 0.5, 0.3]))
+)
+
+V = collect(1:15)
+
+p = ominmax(prob, V; max = true)
+@test p[:, 1] ≈ SparseVector(15, [4, 10], [0.3, 0.7])
+@test p[:, 2] ≈ SparseVector(15, [5, 6, 7], [0.5, 0.3, 0.2])
