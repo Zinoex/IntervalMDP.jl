d62ed0d25602e00c352b78e987db9457756c9835
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-10-17 18:11:38 +0200

    Add randomized benchmark

---
+++ benchmark/problem.jl
@@ -0,0 +1,29 @@
+using Revise, BenchmarkTools, Random, ProgressMeter
+using IMDP, SparseArrays, CUDA
+
+rng = MersenneTwister(55392)
+
+n = 40000
+nnz_per_column = 200
+prob_split = 1/nnz_per_column
+
+q = collect(1:n)
+rand_val_lower = rand(rng, Float64, nnz_per_column * n) .* prob_split
+rand_val_upper = rand(rng, Float64, nnz_per_column * n) .* prob_split .+ prob_split
+
+row_vals = Vector{Int32}(undef, nnz_per_column * n)
+col_ptrs = Int32[1; collect(1:n) .* nnz_per_column .+ 1]
+
+@showprogress for j in 1:n
+    # TODO: Find a faster way to select a small subset
+    rand_index = shuffle!(rng, q)[1:nnz_per_column]
+    sort!(rand_index)
+
+    row_vals[(j - 1) * nnz_per_column + 1:j * nnz_per_column] .= rand_index
+end
+
+lower = SparseMatrixCSC{Float64, Int32}(n, n, col_ptrs, row_vals, rand_val_lower)
+upper = SparseMatrixCSC{Float64, Int32}(n, n, col_ptrs, row_vals, rand_val_upper)
+
+prob = MatrixIntervalProbabilities(;lower=lower, upper=upper)
+V = rand(Float64, n)
