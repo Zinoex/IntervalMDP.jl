244794d57f92dcf1bdfc3cac923b703d75cc6447
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-10-18 11:04:31 +0200

    Add a combined file for benchmarking on clusters

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,11 +1,2 @@
-using Revise, BenchmarkTools
-using IMDP, CUDA
-
-display(@benchmark ominmax!($ordering, $p, $prob, $V; max = true) seconds = 60)
-
-if CUDA.functional()
-    display(
-        @benchmark ominmax!($cuda_ordering, $cuda_p, $cuda_prob, $cuda_V; max = true) seconds =
-            60
-    )
-end
+include("problem.jl")
+include("execute.jl")
