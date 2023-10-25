6241b0f5b5bdae3cc2062324d4110fbfe22217fe
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-10-25 23:02:16 +0200

    Switch to warp-vectorized kernel for even crazier speed-up

--- ext/cuda/probability_assignment.jl
+++ ext/cuda/probability_assignment.jl
@@ -6,8 +6,8 @@ function IMDP.probability_assignment!(
     indices,
 ) where {Tv, Ti}
     copyto!(nonzeros(p), nonzeros(lower(prob)))
-    add_gap_scalar!(p, prob, ordering, indices)
-    # add_gap_vector_kernel!(p, prob, ordering, indices)
+    # add_gap_scalar!(p, prob, ordering, indices)
+    add_gap_vector!(p, prob, ordering, indices)
 
     return p
 end
@@ -54,7 +54,7 @@ function add_gap_scalar_kernel!(
         remaining = one(Tv) - sum_lower[j]
 
         for s in one(Ti):Ti(length(subset))
-            t = subset.offset + subset[s] - Ti(1)
+            t = subset.offset + subset[s] - one(Ti)
 
             gₜ = g_nzs[t]
 
@@ -71,7 +71,7 @@ function add_gap_scalar_kernel!(
     return nothing
 end
 
-function add_gap_vector_kernel!(
+function add_gap_vector!(
     p::CuSparseMatrixCSC{Tv, Ti},
     prob::MatrixIntervalProbabilities{Tv},
     ordering::CuSparseOrdering{Ti},
@@ -102,7 +102,7 @@ function add_gap_vector_kernel!(
 ) where {Tv, Ti, A}
     assume(warpsize() == 32)
 
-    thread_id = (blockIdx().x - Ti(1)) * blockDim().x + threadIdx().x
+    thread_id = (blockIdx().x - one(Ti)) * blockDim().x + threadIdx().x
     warp_id, lane = fldmod1(thread_id, 32)
 
     if warp_id <= length(indices)
@@ -112,49 +112,50 @@ function add_gap_vector_kernel!(
         g_nzs = gap.nzVal
 
         subset = ordering.subsets[j]
+        subset_length = length(subset)
         remaining = one(Tv) - sum_lower[j]
 
         s = one(Ti)
-        while s <= length(subset)
+        while s <= subset_length
             # Find index of the permutation, and lookup the corresponding gap
             sₗ = s + lane - one(Ti)
-            if sₗ <= length(subset)
-                t = subset.offet + subset[sₗ] - Ti(1)
+            if sₗ <= subset_length
+                t = subset.offset + subset[sₗ] - one(Ti)
 
                 g = g_nzs[t]
                 cum_gap = g
             else
                 # 0 gap is a neural element
-                cum_gap = zero(Ti)
+                cum_gap = zero(Tv)
             end
 
             # Cummulatively sum the gap with a tree reduction
-            delta = 1
-            while delta < 32
+            delta = Ti(1)
+            while delta < Ti(32)
                 gₙ = shfl_up_sync(0xffffffff, cum_gap, delta)
                 if lane > delta
                     cum_gap += gₙ
                 end
 
-                delta *= 2
+                delta *= Ti(2)
             end
 
             # Update the remaining probability
-            remaining -= gₜ
-            if sₗ <= length(subset)
+            remaining -= cum_gap
+            if sₗ <= subset_length
                 remaining += g
             end
-            remaining = ifelse(remaining < zero(Tv), zero(Tv), remaining)
+            remaining = max(zero(Tv), remaining)
 
             # Update the probability
-            if sₗ <= length(subset)
+            if sₗ <= subset_length
                 sub = min(g, remaining)
                 p_nzs[t] += sub
                 remaining -= sub
             end
 
             # Update the remaining probability from the last thread in the warp
-            remaining = shfl_sync(0xffffffff, remaining, 31)
+            remaining = shfl_sync(0xffffffff, remaining, Ti(31))
 
             # Early exit if the remaining probability is zero
             if remaining <= zero(Tv)
