79ec2938a2aa1704c85e20458703482c90b4949e
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-10-29 19:57:35 +0100

    Test interval_value_iteration

--- test/ivi.jl
+++ test/ivi.jl
@@ -1,7 +1,8 @@
 # Vector of vectors
 prob1 = StateIntervalProbabilities(; lower = [0.0, 0.1, 0.2], upper = [0.5, 0.6, 0.7])
 prob2 = StateIntervalProbabilities(; lower = [0.5, 0.3, 0.1], upper = [0.7, 0.5, 0.3])
-prob = [prob1, prob2]
+prob3 = StateIntervalProbabilities(; lower = [0.0, 0.0, 1.0], upper = [0.0, 0.0, 1.0])
+prob = [prob1, prob2, prob3]
 
 V = [0.0, 0.0, 1.0]
 
@@ -15,8 +16,8 @@ V_conv, k, last_dV =
 
 # Matrix
 prob = MatrixIntervalProbabilities(;
-    lower = [0.0 0.5; 0.1 0.3; 0.2 0.1],
-    upper = [0.5 0.7; 0.6 0.5; 0.7 0.3],
+    lower = [0.0 0.5 0.0; 0.1 0.3 0.0; 0.2 0.1 1.0],
+    upper = [0.5 0.7 0.0; 0.6 0.5 0.0; 0.7 0.3 1.0],
 )
 
 V = [0.0, 0.0, 1.0]
