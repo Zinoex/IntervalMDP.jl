99982567ec5a5988b5792c3feae9f1abfcf4903d
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-10-31 12:00:46 +0100

    Test IMDP

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Random, StatsBase
 using IMDP, SparseArrays, CUDA, Adapt
 
 @testset "IMDP.jl" begin
-    test_files = ["ominmax.jl", "partial.jl", "ivi.jl"]
+    test_files = ["ominmax.jl", "partial.jl", "ivi.jl", "imdp.jl"]
     for f in test_files
         @testset "$f" begin
             include(f)
