4fe982357a02d7680332d27826308b198dad3946
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-12-22 16:45:50 +0100

    Add JSON as dependency and export correct functions

--- src/Data/Data.jl
+++ src/Data/Data.jl
@@ -1,6 +1,6 @@
 module Data
 using IMDP, SparseArrays
-using NCDatasets
+using NCDatasets, JSON
 
 include("bmdp-tool.jl")
 export read_bmdp_tool_file, write_bmdp_tool_file
@@ -9,6 +9,7 @@ include("prism.jl")
 export read_prism_file, write_prism_file
 
 include("imdp.jl")
-export read_imdp_jl_file, write_imdp_jl_file
+export read_imdp_jl, read_imdp_jl_model, read_imdp_jl_spec
+export write_imdp_jl_model, write_imdp_jl_spec
 
 end
