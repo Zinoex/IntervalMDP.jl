1dbcbe92e0a14c4a89a1e79f04d4c5fefabf61d2
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-05-22 22:30:02 +0200

    Format

--- ext/cuda/ordering.jl
+++ ext/cuda/ordering.jl
@@ -24,7 +24,6 @@ function IntervalMDP.sort_states!(order::CuDenseOrdering, V; max = true)
     return order
 end
 
-
 ###################
 # Sparse ordering #
 ###################
--- test/cuda/dense/bellman.jl
+++ test/cuda/dense/bellman.jl
@@ -1,8 +1,10 @@
 #### Maximization
-prob = IntervalMDP.cu(IntervalProbabilities(;
-    lower = [0.0 0.5; 0.1 0.3; 0.2 0.1],
-    upper = [0.5 0.7; 0.6 0.5; 0.7 0.3],
-))
+prob = IntervalMDP.cu(
+    IntervalProbabilities(;
+        lower = [0.0 0.5; 0.1 0.3; 0.2 0.1],
+        upper = [0.5 0.7; 0.6 0.5; 0.7 0.3],
+    ),
+)
 
 V = IntervalMDP.cu([1.0, 2.0, 3.0])
 
@@ -23,10 +25,12 @@ Vres = Vector(Vres)
 @test Vres ≈ [0.3 * 2 + 0.7 * 3, 0.5 * 1 + 0.3 * 2 + 0.2 * 3]
 
 #### Minimization
-prob = IntervalMDP.cu(IntervalProbabilities(;
-    lower = [0.0 0.5; 0.1 0.3; 0.2 0.1],
-    upper = [0.5 0.7; 0.6 0.5; 0.7 0.3],
-))
+prob = IntervalMDP.cu(
+    IntervalProbabilities(;
+        lower = [0.0 0.5; 0.1 0.3; 0.2 0.1],
+        upper = [0.5 0.7; 0.6 0.5; 0.7 0.3],
+    ),
+)
 
 V = IntervalMDP.cu([1.0, 2.0, 3.0])
 
