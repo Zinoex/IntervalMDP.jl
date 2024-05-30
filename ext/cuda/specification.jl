fe172d917e1e884c0e7a6d4be6f77a3afd069852
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-05-30 14:24:35 +0200

    Apparently sparse CUDA matrices are not subtypes of AbstractGPUMatrix

--- ext/cuda/specification.jl
+++ ext/cuda/specification.jl
@@ -9,12 +9,22 @@ function IntervalMDP.checkdevice!(::AbstractGPUVector, ::AbstractGPUMatrix)
     return nothing
 end
 
-function IntervalMDP.checkdevice!(::AbstractVector, ::AbstractGPUMatrix)
-    # The first argument is on the CPU (technically in RAM) and the second is on the GPU.
-    @assert false "The reward vector is a CPU array and the transition matrix is on the GPU."
+function IntervalMDP.checkdevice!(::AbstractGPUVector, ::AbstractCuSparseMatrix)
+    # Both arguments are on the GPU.
+    return nothing
 end
 
-function IntervalMDP.checkdevice!(::AbstractGPUVector, ::AbstractMatrix)
+function IntervalMDP.checkdevice!(b::AbstractVector, A::AbstractGPUMatrix)
+    # The first argument is on the CPU (technically in RAM) and the second is on the GPU.
+    @assert false "The reward vector is a CPU array ($(typeof(b))) and the transition matrix is on the GPU ($(typeof(A)))."
+end
+
+function IntervalMDP.checkdevice!(b::AbstractVector, A::AbstractCuSparseMatrix)
+    # The first argument is on the CPU (technically in RAM) and the second is on the GPU.
+    @assert false "The reward vector is a CPU array ($(typeof(b))) and the transition matrix is on the GPU ($(typeof(A)))."
+end
+
+function IntervalMDP.checkdevice!(b::AbstractGPUVector, A::AbstractMatrix)
     # The first argument is on the GPU and the second is on the CPU (technically in RAM).
-    @assert false "The reward vector is on the GPU and the transition matrix is a CPU matrix."
+    @assert false "The reward vector is on the GPU ($(typeof(b))) and the transition matrix is a CPU matrix ($(typeof(A)))."
 end
