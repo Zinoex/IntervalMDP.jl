9fba1ef22ac6c2c30099753d2901faabb101d75c
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-06-18 16:03:12 +0200

    Test synthesis for Sequential and MultiDim including actions

--- test/sequential/synthesis.jl
+++ test/sequential/synthesis.jl
@@ -85,14 +85,14 @@ sparse_mdp = IntervalMarkovDecisionProcess(transition_probs)
 product_mdp = ParallelProduct([dense_mdp, sparse_mdp])
 
 # Deterministic MDP
-prob1 = transition_hcat(9, [1, 5])
-prob2 = transition_hcat(9, [2, 8])
+prob1 = transition_hcat(9, [2, 4], [1, 5])
+prob2 = transition_hcat(9, [2, 8], [3, 7])
 prob3 = transition_hcat(9, [3, 9])
 prob4 = transition_hcat(9, [3, 4, 6])
-prob5 = transition_hcat(9, [1, 8])
-prob6 = transition_hcat(9, [4, 5])
+prob5 = transition_hcat(9, [1, 7], [1, 8])
+prob6 = transition_hcat(9, [4, 5], [5, 6])
 prob7 = transition_hcat(9, [6, 7])
-prob8 = transition_hcat(9, [1, 2])
+prob8 = transition_hcat(9, [5, 8], [1, 2])
 prob9 = transition_hcat(9, [9])
 
 transition_probs = [prob1, prob2, prob3, prob4, prob5, prob6, prob7, prob8, prob9]
@@ -110,6 +110,12 @@ problem = Problem(sequential_mdp, spec)
 strategy, V_fixed_it, k, _ = control_synthesis(problem)
 @test k == 10
 
-display(strategy)
-# TODO: Test strategy is correct
-# The deterministic strategy is definitely not correct
+for i in 1:10
+    @test strategy[1][i] == [1, 4, 5, 6, 7, 10, 11, 12, 14]
+    @test strategy[2][2][i] == [1 4 5; 1 4 5; 1 4 5]
+end
+
+@test strategy[2][1][10] == [1 1 1; 3 3 4; 5 5 5]
+for i in 1:9
+    @test strategy[2][1][i] == [1 1 1; 4 4 4; 5 5 5]
+end
