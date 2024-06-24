ebe01f140b2b816e5f6febde1cece5a9002a9423
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-06-24 16:20:31 +0200

    Rename test folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using IntervalMDP, SparseArrays, CUDA
 @testset verbose = true "IntervalMDP.jl" begin
     @testset verbose = true "base" include("base/base.jl")
     @testset verbose = true "sparse" include("sparse/sparse.jl")
-    @testset verbose = true "parallel" include("parallel/parallel.jl")
+    @testset verbose = true "parallel_product" include("parallel_product/parallel.jl")
     @testset verbose = true "data" include("data/data.jl")
 
     if CUDA.functional()
