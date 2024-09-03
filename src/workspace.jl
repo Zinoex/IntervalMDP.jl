2cf33e571e3ed6a0df3bb5bdef2086d834c65adb
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-09-03 10:39:49 +0200

    Add missing field (scratch) to workspace

--- src/workspace.jl
+++ src/workspace.jl
@@ -29,29 +29,31 @@ construct_workspace(prob::IntervalProbabilities, max_actions = 1) =
 
 # Dense
 struct DenseWorkspace{T <: Real}
-    permutation::Vector{Int32}
     scratch::Vector{Int32}
+    permutation::Vector{Int32}
     actions::Vector{T}
 end
 
 function DenseWorkspace(p::AbstractMatrix{T}, max_actions) where {T <: Real}
     n = size(p, 1)
-    perm = Vector{Int32}(undef, n)
     scratch = Vector{Int32}(undef, n)
+    perm = Vector{Int32}(undef, n)
     actions = Vector{T}(undef, max_actions)
-    return DenseWorkspace(perm, scratch, actions)
+    return DenseWorkspace(scratch, perm, actions)
 end
 
 struct ThreadedDenseWorkspace{T <: Real}
+    scratch::Vector{Int32}
     permutation::Vector{Int32}
     actions::Vector{Vector{T}}
 end
 
 function ThreadedDenseWorkspace(p::AbstractMatrix{T}, max_actions) where {T <: Real}
     n = size(p, 1)
+    scratch = Vector{Int32}(undef, n)
     perm = Vector{Int32}(undef, n)
     actions = [Vector{T}(undef, max_actions) for _ in 1:Threads.nthreads()]
-    return ThreadedDenseWorkspace(perm, actions)
+    return ThreadedDenseWorkspace(scratch, perm, actions)
 end
 
 function construct_workspace(p::AbstractMatrix, max_actions; threshold = 10)
@@ -64,17 +66,17 @@ end
 
 # Sparse
 struct SparseWorkspace{T <: Real}
-    values_gaps::Vector{Tuple{T, T}}
     scratch::Vector{Tuple{T, T}}
+    values_gaps::Vector{Tuple{T, T}}
     actions::Vector{T}
 end
 
 function SparseWorkspace(p::AbstractSparseMatrix{T}, max_actions) where {T <: Real}
     max_nonzeros = maximum(map(nnz, eachcol(p)))
-    values_gaps = Vector{Tuple{T, T}}(undef, max_nonzeros)
     scratch = Vector{Tuple{T, T}}(undef, max_nonzeros)
+    values_gaps = Vector{Tuple{T, T}}(undef, max_nonzeros)
     actions = Vector{T}(undef, max_actions)
-    return SparseWorkspace(values_gaps, scratch, actions)
+    return SparseWorkspace(scratch, values_gaps, actions)
 end
 
 struct ThreadedSparseWorkspace{T}
