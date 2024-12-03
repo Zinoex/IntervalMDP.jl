964482c3d1faf0c66534e9a35df36fe1db3b0abd
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-12-03 22:16:15 +0100

    Add given strategy to CUDA dense

--- test/cuda/dense/synthesis.jl
+++ test/cuda/dense/synthesis.jl
@@ -41,23 +41,30 @@ transition_probs = [prob1, prob2, prob3]
 istates = [Int32(1)]
 
 mdp = IntervalMarkovDecisionProcess(transition_probs, istates)
+mdp = IntervalMDP.cu(mdp)
 
+# Finite time reachability
 prop = FiniteTimeReachability([3], 10)
 spec = Specification(prop, Pessimistic, Maximize)
 problem = Problem(mdp, spec)
-problem = IntervalMDP.cu(problem)
-
 policy, V, k, res = control_synthesis(problem)
 
+@test policy isa TimeVaryingStrategy
 @test time_length(policy) == 10
 for k in 1:time_length(policy)
     @test Vector(policy[k]) == [1, 2, 1]
 end
 
+# Check if the value iteration for the IMDP with the policy applied is the same as the value iteration for the original IMDP
+problem = Problem(mdp, spec, policy)
+V_mc, k, res = value_iteration(problem)
+@test V ≈ V_mc
+
+# Finite time reward
 prop = FiniteTimeReward([2.0, 1.0, 0.0], 0.9, 10)
+prop = IntervalMDP.cu(prop)
 spec = Specification(prop, Pessimistic, Maximize)
 problem = Problem(mdp, spec)
-problem = IntervalMDP.cu(problem)
 
 policy, V, k, res = control_synthesis(problem)
 
@@ -66,10 +73,39 @@ for k in 1:time_length(policy)
     @test Vector(policy[k]) == [2, 2, 1]
 end
 
+# Check if the value iteration for the IMDP with the policy applied is the same as the value iteration for the original IMDP
+problem = Problem(mdp, spec, policy)
+V_mc, k, res = value_iteration(problem)
+@test V ≈ V_mc
+
+# Infinite time reachability
 prop = InfiniteTimeReachability([3], 1e-6)
 spec = Specification(prop, Pessimistic, Maximize)
 problem = Problem(mdp, spec)
-problem = IntervalMDP.cu(problem)
-
 policy, V, k, res = control_synthesis(problem)
-@test Vector(policy[1]) == [1, 2, 1]
+
+@test policy isa StationaryStrategy
+@test IntervalMDP.cpu(policy)[1] == [1, 2, 1]
+
+# Check if the value iteration for the IMDP with the policy applied is the same as the value iteration for the original IMDP
+problem = Problem(mdp, spec, policy)
+V_mc, k, res = value_iteration(problem)
+@test V ≈ V_mc
+
+# Finite time safety
+prop = FiniteTimeSafety([3], 10)
+spec = Specification(prop, Pessimistic, Maximize)
+problem = Problem(mdp, spec)
+policy, V, k, res = control_synthesis(problem)
+
+@test all(V .>= 0.0)
+@test CUDA.@allowscalar(V[3]) ≈ 0.0
+
+@test policy isa TimeVaryingStrategy
+@test time_length(policy) == 10
+for k in 1:(time_length(policy) - 1)
+    @test Vector(policy[k]) == [2, 2, 1]
+end
+
+# The last time step (aka. the first value iteration step) has a different strategy.
+@test Vector(policy[time_length(policy)]) == [2, 1, 1]
