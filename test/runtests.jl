a3840f6d07da5d878edb2776ce6540ad21f34ab9
Frederik Baymler Mathiesen <frederik@baymler.com>
2025-07-16 15:07:58 +0200

    Format

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Test
     @testset verbose = true "base" include("base/base.jl")
     @testset verbose = true "sparse" include("sparse/sparse.jl")
     @testset verbose = true "data" include("data/data.jl")
-    # @testset verbose = true "cuda" include("cuda/cuda.jl")
+    @testset verbose = true "cuda" include("cuda/cuda.jl")
 end
