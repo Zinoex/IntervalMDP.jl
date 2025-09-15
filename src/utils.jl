dc406c8e91497e42dd49f80d039c072d9c6f8366
Frederik Baymler Mathiesen <frederik@baymler.com>
2025-09-15 09:48:37 +0200

    Fix valuetype

--- src/utils.jl
+++ src/utils.jl
@@ -8,10 +8,24 @@ arrayfactory(prob::IntervalAmbiguitySets, T, sizes) =
     arrayfactory(prob.gap, T, sizes)
 arrayfactory(::MR, T, sizes) where {MR <: AbstractArray} = Array{T}(undef, sizes)
 
+function valuetype(prob::AbstractIntervalMDPProblem)
+    spec_valuetype = valuetype(specification(prob))
+    sys_valuetype = valuetype(system(prob))
+
+    if isnothing(spec_valuetype)
+        return sys_valuetype
+    end
+
+    return promote_type(spec_valuetype, sys_valuetype)
+end
+valuetype(spec::Specification) = valuetype(system_property(spec))
+
 valuetype(mp::ProductProcess) = valuetype(markov_process(mp))
 valuetype(mp::FactoredRMDP) = promote_type(valuetype.(marginals(mp))...)
 valuetype(marginal::Marginal) = valuetype(ambiguity_sets(marginal))
 valuetype(::IntervalAmbiguitySets{R}) where {R} = R
 valuetype(::AbstractArray{R}) where {R} = R
+
+valuetype(::Property) = nothing
 valuetype(::FiniteTimeReward{R}) where {R} = R
 valuetype(::InfiniteTimeReward{R}) where {R} = R
